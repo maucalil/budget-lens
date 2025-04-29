b3df59b3c15f14d763228c9a6ec8225a3de1066d
Maurício Calil Manfrim Lucera <mauricio.calil@usp.br>
2025-04-29 15:55:35 -0300

    chore(shared): reorganize folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { SidebarComponent } from '@shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
