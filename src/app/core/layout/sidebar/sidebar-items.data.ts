aea1aa4e9ebd593be2303c6a08819847b6e46b2f
Maurício Calil Manfrim Lucera <mauricio.calil@usp.br>
2025-05-01 16:58:37 -0300

    feat: create main features base files and add to routes

--- src/app/core/layout/sidebar/sidebar-items.data.ts
+++ src/app/core/layout/sidebar/sidebar-items.data.ts
@@ -2,13 +2,18 @@ import { SidebarItem } from './sidebar-item.interface';
 
 export const sidebarItems: SidebarItem[] = [
   {
-    link: '/',
-    icon: 'fa fa-home',
+    link: '',
+    icon: 'fa-house-user', // chart-line
     label: 'Dashboard',
   },
   {
-    link: '/teste',
-    icon: 'fa fa-home',
-    label: 'Teste',
+    link: '/contas',
+    icon: 'fa-credit-card',
+    label: 'Contas',
+  },
+  {
+    link: '/transacoes',
+    icon: 'fa-money-bill-wave', // money-check-dollar
+    label: 'Transacoes',
   },
 ];
