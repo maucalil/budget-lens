99959245fb5968c5ac982c6079ef5be300d274a2
larissa <99204156+lal4mp@users.noreply.github.com>
2025-06-05 21:13:17 -0300

    refactor(button): refactor button component classes

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,11 +1,10 @@
-import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconProp } from 'node_modules/@fortawesome/angular-fontawesome/types';
 
 @Component({
   selector: 'app-button',
-  imports: [FontAwesomeModule, NgClass],
+  imports: [FontAwesomeModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
