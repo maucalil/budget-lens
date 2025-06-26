0bb6f8933b45c0e3e7f80cabe28c224d0dcbad04
Maurício Calil Manfrim Lucera <m.calil-manfrim-lucera@tu-braunschweig.de>
2025-06-26 14:24:16 -0300

    fix(environments): add api routes prefix to api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://budget-lens-api-production.up.railway.app',
+  apiUrl: 'https://budget-lens-api-production.up.railway.app/api/v1',
 };
