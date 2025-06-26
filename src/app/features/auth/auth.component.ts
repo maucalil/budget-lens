3b81885c59f2fd86ffd1178cadfe09ffd3bcb0c5
Maurício Calil Manfrim Lucera <m.calil-manfrim-lucera@tu-braunschweig.de>
2025-06-26 18:43:53 -0300

    feat(authenticate): add loading state for login/register page

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { SnackbarComponent } from '@core/layout/snackbar/snackbar.component';
+import { LoaderService } from '@core/services';
+import { AsyncPipe } from '@angular/common';
+import { SpinnerComponent } from '@shared/components';
 
 @Component({
   selector: 'app-auth',
-  imports: [LoginComponent, RegisterComponent, SnackbarComponent],
+  imports: [
+    LoginComponent,
+    RegisterComponent,
+    SnackbarComponent,
+    SpinnerComponent,
+    AsyncPipe,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss',
 })
 export class AuthComponent {
+  loading$ = inject(LoaderService).loading$;
+
   showLogin = true;
 
   switchToRegister(): void {
